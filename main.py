13804a9305e73b01f811f547aa1e2fde56427b54
aidan <aidankennedy47@gmail.com>
2025-07-05 14:47:50 +1000

    can now search the subreddit

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-#test commit
+from dotenv import load_dotenv
+import os
+import praw
+
+load_dotenv()
+
+reddit = praw.Reddit(
+    client_id=os.getenv("REDDIT_CLIENT_ID"),          # from your Reddit app
+    client_secret=os.getenv("REDDIT_CLIENT_SECRET"),  # from your Reddit app
+    user_agent='Stock ticker counter of recent 100 posts by AcanthaceaePristine7'  # any descriptive string
+)
+
+subreddit = reddit.subreddit('pennystocks')
+
+for post in subreddit.top(limit=10, time_filter='month'):
+    print(post.title)
